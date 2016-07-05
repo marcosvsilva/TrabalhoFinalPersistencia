7068fc9fee1b2f72df4a838e1514f309ab5462ad
Marcos Vinicius <marcos.v.silva@live.com>
2016-07-05 15:07:29 -0300

    Drives de conexão

---
+++ src/Conexao/ConexaoBD.java
@@ -0,0 +1,23 @@
+package Conexao;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import org.mongojack.JacksonDBCollection;
+import org.mongojack.WriteResult;
+
+
+/**
+ * Created by Marcos on 05/07/2016.
+ */
+
+public class ConexaoBD
+{
+
+    protected dataBase mongoDataBase;
+    protected dataBaseCollection mongocolCollection;
+
+
+
+
+
+}
--- src/ConexaoBD.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Marcos on 05/07/2016.
- */
-public class ConexaoBD {
-}
